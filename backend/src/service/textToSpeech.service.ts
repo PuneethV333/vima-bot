a0f8584f93aab64e4efd7d6c936ad063e84c133c
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-17 15:21:38 +0530

    add:new voice

--- backend/src/service/textToSpeech.service.ts
+++ backend/src/service/textToSpeech.service.ts
@@ -3,7 +3,7 @@ import { config } from "../config/data.config"
 
 export const textToSpeech = async (text: string) => {
     const url = `${config.fastUrl}/api/whisper/speak`
-    const res = await axios.post(url, { text, voice: "en-IN-PrabhatNeural" }, {
+    const res = await axios.post(url, { text, voice: "en-IN-NeerjaNeural" }, {
         responseType: "arraybuffer"
     })
     return Buffer.from(res.data).toString("base64")
