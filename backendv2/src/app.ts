79b6b495f52e90e6d5f4b69d79d98d694df3f89c
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-26 15:27:41 +0530

    start:v2

--- backendv2/src/app.ts
+++ backendv2/src/app.ts
@@ -4,9 +4,6 @@ import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import morgan from "morgan"
 import cors from "cors"
-import { config } from "./config/data.config"
-import { errorHandling } from "./middleware/error.middleware";
-import { chatRouter } from "./routes/chat.router";
 
 export const app = express()
 
@@ -24,17 +21,6 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 
-app.use(
-    cors({
-        origin: config.frontendUrl,
-        credentials: true,
-    }),
-);
-
 app.get("/test", (_: Request, res: Response) => {
     res.send("Server is running");
 });
-
-app.use("/api/chat",chatRouter)
-
-app.use(errorHandling)
